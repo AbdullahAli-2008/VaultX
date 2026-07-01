2910feb4cf4d86a64a27a90eca5c5e05e63743be
Abdullah Ali <AbdullahAli.2008@outlook.com>
2026-07-01 20:29:01 +0500

    Logo with and without background added

---
+++ src/ui/LoginScreen.cpp
@@ -0,0 +1,16 @@
+#include "LoginScreen.h"
+using namespace std;
+
+LoginScreen::LoginScreen() {
+    password = "password123";
+    passwordInput = "";
+    showError = true;
+}
+
+AppScreen LoginScreen::update(){
+
+}
+
+void LoginScreen::draw(){
+    
+}
